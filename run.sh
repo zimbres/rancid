9785cb0d8df8a3802aa7bda0be3da9c6b6290fd9
Marcio <marcio@zimbres.com>
2024-07-14 15:41:40 -0300

    Fix check directory is empty

--- run.sh
+++ run.sh
@@ -5,15 +5,21 @@ if [ -z "$REPO" ]; then
     exit 1
 fi
 
-if ! git -C /home/rancid/rancid/var rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+DIR="/home/rancid/rancid/var"
+
+if ! git -C "$DIR" rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo "Not a git repo. Initializing..."
-    if [ -f /home/rancid/rancid/var/.gitkeep ]; then
-        rm /home/rancid/rancid/var/.gitkeep
+
+    if [ -n "$(ls -A "$DIR")" ]; then
+        echo "Directory is not empty. Deleting contents..."
+        rm -rf "$DIR"/* 2>/dev/null
+        rm "$DIR"/.* 2>/dev/null
     fi
-    git clone $REPO /home/rancid/rancid/var
+
+    git clone $REPO "$DIR"
 fi
 
 /home/rancid/rancid/bin/rancid-run
 
 echo "$(date): Running 'cleanup logs'"
-find /home/rancid/rancid/var/logs/* -mtime +0 -exec rm -f {} \;
+find "$DIR"/logs/* -mtime +0 -exec rm -f {} \;
