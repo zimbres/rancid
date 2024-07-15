15336b5984ba3e56211f1b3678b0674e2f1f5e99
Marcio <marcio@zimbres.com>
2024-07-15 14:47:47 -0300

    Update run script and Output from rancid-run

--- run.sh
+++ run.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
+echo "Starting..."
 
 if [ -z "$REPO" ]; then
-    echo "Error: REPO variable is not set. Exiting."
+    echo ""
+    echo "[$(date)]: Error: 'REPO' variable is not set! Exiting..."
+    echo ""
     exit 1
 fi
 
 DIR="/home/rancid/rancid/var"
 
 if ! git -C "$DIR" rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    echo "Not a git repo. Initializing..."
+    echo ""
+    echo "[$(date)]: Not a git repo! Initializing..."
+    echo ""
 
     if [ -n "$(ls -A "$DIR")" ]; then
-        echo "Directory is not empty. Deleting contents..."
+        echo ""
+        echo "[$(date)]: Directory is not empty! Deleting contents..."
         rm -rf "$DIR"/* 2>/dev/null
         rm "$DIR"/.* 2>/dev/null
+        echo ""
     fi
-
+    echo ""
     git clone $REPO "$DIR"
+    echo ""
 else
-    echo "Repo already initialized. Skiping..."
+    echo ""
+    echo "[$(date)]: Repo already initialized! Skiping..."
+    echo ""
 fi
 
+echo ""
+echo "[$(date)]: Running 'runcid'..."
+echo ""
 /home/rancid/rancid/bin/rancid-run
+echo ""
 
-echo "$(date): Running 'cleanup logs'"
+echo ""
+echo "[$(date)]: Running 'cleanup logs'..."
 find "$DIR"/logs/* -mtime +0 -exec rm -f {} \;
+echo ""
+
+echo "End."
